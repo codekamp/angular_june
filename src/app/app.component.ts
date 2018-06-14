039846cd73db8dce63e3538f55e21c02daf8755a
Prashant Chaudhary <101.prashant@gmail.com>
2018-06-14 13:36:48 +0530

    rating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  caption = 'Hello World';
+
+  courseRating: number = 3;
+  teacherRating: number = 1;
+
+  constructor() {
+    // setInterval(() => {
+    //   this.caption = this.caption + ' ' + this.caption;
+    // }, 2000);
+  }
+
+  saveFeedback() {
+    console.log("courseRating", this.courseRating);
+    console.log("teachRating", this.teacherRating);
+  }
+
+  updateCourseRating(newRating: number) {
+    this.courseRating = newRating;
+
+    console.log("new courseRating", newRating);
+  }
+
+
+  updateTeacherRating(newRating: number) {
+    this.teacherRating = newRating;
+
+    console.log("new teacherRating", newRating);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {ImgCaptionComponent} from './img-caption/img-caption.component';
+import { LoginComponent } from './login/login.component';
+import { FiveStartComponent } from './five-start/five-start.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCaptionComponent,
+    LoginComponent,
+    FiveStartComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule
