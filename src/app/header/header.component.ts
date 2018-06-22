27a13ec9d32f0a18439b66cb2dc176bb1a89ec41
Prashant Chaudhary <101.prashant@gmail.com>
2018-06-22 13:44:46 +0530

    observables

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [`
-    .header {
-      justify-content: space-around;
-    }
-  `]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
@@ -17,3 +13,12 @@ export class HeaderComponent implements OnInit {
   }
 
 }
+
+
+// ViewEncapsulation.None - inner css will impact outside world and vice versa
+
+// ViewEncapsulation.Emulated - This is default value. Outside css will impact inner
+// ... but inner won't impact outside. Random attributes are added to achieve this
+
+// ViewEncapsulation.Native - Ouside won't impact inside and vice versa.
+// /// uses ShadowDom, which is not available in all browsers.
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,9 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +12,51 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  // usernameField.nativeElement is same as pqr variable of template
+  // @ViewChild('pqr') usernameField: any;
+
+  // passwordField.nativeElement is same as xyz variable of template
+  // @ViewChild('xyz') passwordField: any;
+
+
+  usernameControl = new FormControl();
+  passwordControl = new FormControl();
+
+  myValue: string;
+
   constructor() { }
 
   ngOnInit() {
+    // setInterval(() => {
+    //   console.log('Data from form is ', this.usernameField.nativeElement.value, this.passwordField.nativeElement.value);
+    // }, 3000);
+
+    //
+    // setInterval(() => {
+    //   console.log('Data from form is ', this.usernameControl.value, this.passwordControl.value);
+    // }, 3000);
+
+    this.usernameControl.valueChanges
+      .filter(username => username.length > 3)
+      .debounceTime(2000)
+      .distinctUntilChanged()
+      .map(username => 'Dear ' + username)
+      .subscribe(filteredUsername => {
+        console.log("Search results for " + filteredUsername);
+        this.myValue = filteredUsername;
+      });
   }
 
-  login(username:string, password:string) {
-    console.log("form submitted", username, password);
+  login(username:any, password:any) {
+    console.log("form submitted", username.value, password.value);
+
+    username.value = '';
+    password.value = '';
   }
 
   goToSignup() {
   }
 }
+
+// http://reactivex.io/documentation/operators.html
+// http://rxmarbles.com/
